e713b334a808769bf4f197f22f89b49b09280f91
trudieis <trudieis@student.21-school.ru>
2023-04-12 10:22:18 +0300

    create matrix and remove matrix

--- s21_create_matrix.c
+++ s21_create_matrix.c
@@ -1,14 +1,32 @@
 #include "s21_matrix.h"
 
 int s21_create_matrix(int rows, int columns, matrix_t *result) {
-  int status = 1;
-  if (rows < 0 || columns < 0) {
-    status = 0;
-  } else {
-    result->matrix =
-        (double **)calloc((double)rows * (double)columns * (sizeof(double)));
+  int status = 1, i = 0;
+  result->matrix = NULL;
+  if (0 < rows && 0 < columns) {
+    result->matrix = calloc(rows, sizeof(double *));
+    if (NULL != result->matrix) {
+      for (; i < rows; ++i) {
+        result->matrix[i] = calloc(columns, sizeof(double));
+      }
+    }
     result->rows = rows;
     result->columns = columns;
+  } else {
+    status = 0;
   }
   return (status = 1) ? SUCCESS : FAILURE;
+}
+
+void s21_remove_matrix(matrix_t *A) {
+  int i = 0;
+  if (NULL != A->matrix) {
+    for (; i < A->rows; ++i) {
+      free(A->matrix[i]);
+    }
+    free(A->matrix);
+    A->rows = 0;
+    A->columns = 0;
+    A = NULL;
+  }
 }
