e65d2ebec65e51676840121690c9b1386737c3bd
trudieis <trudieis@student.21-school.ru>
2023-04-12 14:08:04 +0300

    try equality

--- src/s21_eq_matrix.c
+++ src/s21_eq_matrix.c
@@ -4,13 +4,13 @@ int s21_eq_matrix(matrix_t *A, matrix_t *B) {
   int status = 1;
   int i = 0, j = 0;
   if (NULL == A->matrix || NULL == B->matrix) {
-    return err_matrix;
+    status = 0;
   } else if (A->rows != B->rows || A->columns != B->columns) {
-    return err_matrix;
+    status = 0;
   } else {
     for (; i < A->rows; ++i) {
       for (; j < A->columns; ++j) {
-        if (A->matrix[i][j] != B->matrix[i][j]) {
+        if (abs(A->matrix[i][j] - B->matrix[i][j]) < 0.0000001) {
           status = 0;
           break;
         }
