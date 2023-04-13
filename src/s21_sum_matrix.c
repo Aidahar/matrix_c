5aaf6ffca5c7c18304bba5ede7851f399e6d3488
trudieis <trudieis@student.21-school.ru>
2023-04-13 08:43:42 +0300

    add new files

--- src/s21_sum_matrix.c
+++ src/s21_sum_matrix.c
@@ -2,10 +2,15 @@
 
 int s21_sum_matrix(matrix_t *A, matrix_t *B, matrix_t *result) {
   int status = 1, idx = 0, jdx = 0;
-  if (NULL == A || NULL == B) {
+  if (NULL == A || NULL == B || NULL == result) {
     status = err_matrix;
-  } else if ((A->rows != B->rows) || (A->columns != B->columns)) {
+  } else if ((A->rows != B->rows) || (A->columns != B->columns) || (result->rows != A->rows) || (result->columns != A->columns)) {
     status = err_matrix;
   } else {
+    for (; idx < A->rows; ++idx) {
+      for (; jdx < A->columns; ++jdx) {
+        result->matrix[idx][jdx] = A->matrix[idx][jdx] + B->matrix[idx][jdx];
+      }
     }
+  }
 }
