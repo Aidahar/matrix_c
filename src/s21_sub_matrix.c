f2a6001733522be7d27dff7c2728f89f8cc8c323
trudieis <trudieis@student.21-school.ru>
2023-04-13 23:08:05 +0300

    add sub

---
+++ src/s21_sub_matrix.c
@@ -0,0 +1,20 @@
+#include "s21_matrix.h"
+
+int s21_sum_matrix(matrix_t *A, matrix_t *B, matrix_t *result) {
+  int status = ok; 
+  if (valid_matrix(A) && valid_matrix(B) && eq_size(A, B)) { 
+    if (!s21_create_matrix(A->rows, A->columns, result)) {
+      int idx, jdx;
+      for (idx = 0; idx < A->rows; ++idx) {
+        for (jdx = 0; jdx < A->columns; ++jdx) {
+          result->matrix[idx][jdx] = A->matrix[idx][jdx] - B->matrix[idx][jdx];
+        }
+      }
+    } else {
+      status = err_matrix;
+    }
+  } else {
+    status = err_matrix;
+  } 
+  return status;
+}
