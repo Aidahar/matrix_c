2d5e7460f739b47b0f9d70a591ae71668093c78b
trudieis <trudieis@student.21-school.ru>
2023-04-19 09:01:47 +0300

    calc compliments fix

--- src/s21_calc_compliments.c
+++ src/s21_calc_compliments.c
@@ -3,7 +3,7 @@
 int minor_mx(int i, int j, matrix_t *A, matrix_t *result) {
   int status = ok, idx, jdx;
   if(valid_matrix(A) && (A->rows == A->columns)) {
-    if (!s21_create_matrix(A->rows, A->columns, &result)) {
+    if (!s21_create_matrix(A->rows - 1, A->columns - 1, &result)) {
       for(idx = 0; idx < A->rows; ++idx) {
         for (jdx = 0; jdx < A->columns; ++jdx) {
           if (idx != i && jdx != j) {
